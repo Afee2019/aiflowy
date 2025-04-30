5115190134da9fc2de79825e8c6856eb87e017a2
18586361686 <1207669650@qq.com>
2025-04-30 15:36:17 +0800

    chore: 修改插件查询表名

--- aiflowy-ui-react/src/pages/ai/botDesign/Plugins.tsx
+++ aiflowy-ui-react/src/pages/ai/botDesign/Plugins.tsx
@@ -4,6 +4,6 @@ import {BotModal, BotModalProps} from "./BotModal.tsx";
 export type PluginsModalProps = Omit<BotModalProps, 'tableAlias' | 'tableTitle'>;
 export const PluginModal: React.FC<PluginsModalProps> = (props) => {
     return (
-        <BotModal tableAlias="aiPlugins" tableTitle="插件"{...props}   />
+        <BotModal tableAlias="aiPlugin" tableTitle="插件"{...props}   />
     );
 }
