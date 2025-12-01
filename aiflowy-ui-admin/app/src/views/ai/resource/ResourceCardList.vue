57c73523cdccedfc8cca33809de799384e2aa4d9
john <409225776@qq.com>
2025-12-01 15:17:39 +0800

    feat: 工作流资源选择组件

--- aiflowy-ui-admin/app/src/views/ai/resource/ResourceCardList.vue
+++ aiflowy-ui-admin/app/src/views/ai/resource/ResourceCardList.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import {onMounted, ref, watch} from 'vue';
 
+import {
+  ElCard,
+  ElCheckbox,
+  ElCol,
+  ElImage,
+  ElRadio,
+  ElRow,
+  ElText,
+  ElTooltip,
+  ElTag,
+} from 'element-plus';
+
+import audioIcon from '#/assets/ai/resource/audio-icon.png';
+import docIcon from '#/assets/ai/resource/doc-icon.png';
+import otherIcon from '#/assets/ai/resource/other-icon.png';
+import videoIcon from '#/assets/ai/resource/video-icon.png';
 import { $t } from '#/locales';
-
-import { ElCard, ElCheckbox, ElCol, ElRadio, ElRow } from 'element-plus';
+import PreviewModal from '#/views/ai/resource/PreviewModal.vue';
+import {useDictStore} from "#/store";
 
 export interface ResourceCardProps {
   data: any[];
@@ -14,9 +30,16 @@ const props = withDefaults(defineProps<ResourceCardProps>(), {
   multiple: false,
   valueProp: 'id',
 });
-
+onMounted(() => {
+  initDict();
+});
+const dictStore = useDictStore();
+function initDict() {
+  dictStore.fetchDictionary('resourceType');
+  dictStore.fetchDictionary('resourceOriginType');
+}
 const emit = defineEmits(['update:modelValue']);
-
+const previewDialog = ref();
 const radioValue = ref('');
 const checkAll = ref(false);
 function choose() {
@@ -49,6 +72,60 @@ function handleCheckAllChange(val: any) {
     }
   }
 }
+function getSrc(item: any) {
+  switch (item.resourceType) {
+    case 0: {
+      return item.resourceUrl;
+    }
+    case 1: {
+      return audioIcon;
+    }
+    case 2: {
+      return videoIcon;
+    }
+    case 3: {
+      return docIcon;
+    }
+    default: {
+      return otherIcon;
+    }
+  }
+}
+function preview(row: any) {
+  previewDialog.value.openDialog({ ...row });
+}
+function getResourceTypeColor(item: any) {
+  switch (item.resourceType) {
+    case 0: {
+      return '#0066FF';
+    }
+    case 1: {
+      return '#FFA200';
+    }
+    case 2: {
+      return '#5600FF';
+    }
+    case 3: {
+      return '#0099CC';
+    }
+    default: {
+      return '#757575';
+    }
+  }
+}
+function getResourceOriginColor(item: any) {
+  switch (item.origin) {
+    case 0: {
+      return '#00B8A9';
+    }
+    case 1: {
+      return '#0066FF';
+    }
+    default: {
+      return '';
+    }
+  }
+}
 watch(
   [() => radioValue.value, () => props.data],
   () => {
@@ -60,6 +137,7 @@ watch(
 
 <template>
   <div>
+    <PreviewModal ref="previewDialog" />
     <ElCheckbox
       v-if="multiple"
       :label="$t('button.selectAll')"
@@ -69,19 +147,45 @@ watch(
     <ElRow :gutter="20">
       <ElCol :span="6" v-for="item in data" :key="item.id" class="mb-5">
         <ElCard
-          :body-style="{ padding: '12px', height: '220px' }"
+          :body-style="{ padding: '12px', height: '285px' }"
           shadow="hover"
         >
-          <template #header>
-            <ElCheckbox
-              v-if="multiple"
-              v-model="item.checkboxValue"
-              :true-value="item[valueProp]"
-              false-value=""
-            />
-            <ElRadio v-else v-model="radioValue" :value="item[valueProp]" />
-          </template>
-          {{ item.resourceName }}
+          <div>
+            <div>
+              <ElCheckbox
+                v-if="multiple"
+                v-model="item.checkboxValue"
+                :true-value="item[valueProp]"
+                false-value=""
+              />
+              <ElRadio v-else v-model="radioValue" :value="item[valueProp]" />
+            </div>
+            <div>
+              <ElImage
+                @click="preview(item)"
+                :src="getSrc(item)"
+                style="width: 100%; height: 150px; cursor: pointer"
+              />
+            </div>
+            <div>
+              <ElTooltip
+                :content="`${item.resourceName}.${item.suffix}`"
+                placement="top"
+              >
+                <ElText truncated>
+                  {{ item.resourceName }}.{{ item.suffix }}
+                </ElText>
+              </ElTooltip>
+            </div>
+            <div class="flex gap-1.5">
+              <ElTag :color="getResourceOriginColor(item)" effect="dark">
+                {{ dictStore.getDictLabel('resourceOriginType', item.origin) }}
+              </ElTag>
+              <ElTag :color="getResourceTypeColor(item)" effect="dark">
+                {{ dictStore.getDictLabel('resourceType', item.resourceType) }}
+              </ElTag>
+            </div>
+          </div>
         </ElCard>
       </ElCol>
     </ElRow>
